68b31a8adbfefbd4098e8dc289090d7fba2c3e02
vincent courtalon <vincent.courtalon@gmail.com>
2017-07-05 10:01:50 +0200

    suite introduction exercices

--- semaine_[04-07-2017_10-07-2017][JAVA]/fonctionsJavaform/src/fonctionsJavaform/Program.java
+++ semaine_[04-07-2017_10-07-2017][JAVA]/fonctionsJavaform/src/fonctionsJavaform/Program.java
@@ -1,5 +1,6 @@
 package fonctionsJavaform;
 
+import java.io.File;
 import java.util.Scanner;
 
 public class Program {
@@ -26,8 +27,23 @@ public class Program {
 		echange(tab);
 		System.out.println("[" + tab[0]  + ", " + tab[1] + "]");
 		
+		System.out.println(factorielle(6));
+		
+		parcourRep(new File("C:\\Users\\Stagiaire\\depotgit"), 0);
+		
+		System.out.println(moyenne(12.5, 18.6, 8.9, 7.5));
 		
 	}
+	
+	public static double moyenne(double ... valeurs) {
+		double somme = 0.0;
+		for (double d : valeurs) {
+			somme += d;
+		}
+		return somme / valeurs.length;
+	}
+	
+	
 	// ici, le type de retour de cette methode est void -> vide,
 	// pas de valeur retournée
 	// le nom de la fonction, avec les même regles de nommage
@@ -69,6 +85,33 @@ public class Program {
 		values[1] = z;
 		//values = new int[5];
 	}
+	// fonction recursive
+	public static long factorielle(long n) {
+		if (n <= 1)
+			return 1;
+		else 
+			return n * factorielle(n - 1);
+	}
+	
+	public static void parcourRep(File rep, int profondeurActuelle) {
+		// récupère la liste des fichiers
+		File[] files = rep.listFiles();
+		for (File f : files) {
+			for (int i = 0; i < profondeurActuelle; i++) {
+				System.out.print("   ");
+			}
+			if (f.isDirectory()) {
+				System.out.println("REP  : " + f.getName());
+				if (profondeurActuelle < 3) {
+					parcourRep(f, profondeurActuelle + 1);
+				}
+			}
+			else {
+				System.out.println("FILE : " + f.getName());
+			}
+		}
+	}
+	
 	
 
 }
